6a661f280ebce20f407764be4f4ee61b4ef4fd13
joknarf <joknarf@free.fr>
2024-06-29 16:34:17 +0200

    symbol comment

--- ssh_para/ssh_para.py
+++ ssh_para/ssh_para.py
@@ -22,10 +22,10 @@ from colorama import Fore, Style, init
 
 os.environ["TERM"] = "xterm-256color"
 
-SYMBOL_END = os.environ.get("SSHP_SYM_BEG") or "\ue0b4"
-SYMBOL_BEGIN = os.environ.get("SSHP_SYM_END") or "\ue0b6"
-SYMBOL_PROG = os.environ.get("SSHP_SYM_PROG") or "\u25a0"
-SYMBOL_RES = os.environ.get("SSHP_SYM_RES") or "\u25b6"  # "\ue0b0"
+SYMBOL_END = os.environ.get("SSHP_SYM_BEG") or "\ue0b4"   # 
+SYMBOL_BEGIN = os.environ.get("SSHP_SYM_END") or "\ue0b6" # 
+SYMBOL_PROG = os.environ.get("SSHP_SYM_PROG") or "\u25a0" # ■
+SYMBOL_RES = os.environ.get("SSHP_SYM_RES") or "\u25b6"   # ▶ 
 DNS_DOMAINS = os.environ.get("SSHP_DOMAINS") or ""
 SSH_OPTS = os.environ.get("SSHP_OPTS") or ""
 
