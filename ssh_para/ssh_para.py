ceb3dddb9d90a4fbbb1c977f893483c6db393712
joknarf <joknarf@free.fr>
2025-09-05 09:07:08 +0200

    fix f string quotes

--- ssh_para/ssh_para.py
+++ ssh_para/ssh_para.py
@@ -519,7 +519,7 @@ class JobPrint(threading.Thread):
             ],
         )
         printfile(
-            f"begin: {datetime.fromtimestamp(self.startsec).strftime("%Y-%m-%d %H:%M:%S")}",
+            f"begin: {datetime.fromtimestamp(self.startsec).strftime('%Y-%m-%d %H:%M:%S')}",
             f"end: --:--:--",
             f"dur: {total_dur}",
             f"runs: {nbend}/{self.nbjobs}",
